48298243a54c43aa1fd333aa4cd598bb0bf32d61
J.D. Thomason <joseph.thomason@snhu.edu>
2018-11-05 15:50:42 -0700

    Added shell for leaderboard command

---
+++ cmds/leaderboard.py
@@ -0,0 +1,10 @@
+
+command = "leaderboard"
+public = True
+
+
+def execute(command, user):
+    attachment = None
+    response = "This is a fancy upcoming feature.  Stay tuned for more!"
+
+    return response, attachment
